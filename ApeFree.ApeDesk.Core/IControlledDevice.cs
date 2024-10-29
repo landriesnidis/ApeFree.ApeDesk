f8953026d7f2eae27656cbda1311e2d25db590ed
Guijie Lee <landriesnidis@yeah.net>
2024-10-29 15:56:56 +0800

    新增日志监视功能

--- ApeFree.ApeDesk.Core/IControlledDevice.cs
+++ ApeFree.ApeDesk.Core/IControlledDevice.cs
@@ -5,7 +5,7 @@ namespace ApeFree.ApeDesk.Core
     /// <summary>
     /// 受控设备接口
     /// </summary>
-    public interface IControlledDevice : IService, IScreenController, IScreenSynchronizer, IDriveBrowser, IProcessManager
+    public interface IControlledDevice : IService, IScreenController, IScreenSynchronizer, IDriveBrowser, IProcessManager, ILogFileMonitor
     {
 
     }
---
+++ ApeFree.ApeDesk.Core/ILogFileMonitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ApeFree.ApeDesk.Core
+{
+    public interface ILogFileMonitor : ApeRpc.IService
+    {
+        void AddFile(string filename);
+        void RemoveFile(string filename);
+
+        event EventHandler<LogUpdatedEventArgs> LogUpdated;
+    }
+
+
+    public class LogUpdatedEventArgs : EventArgs
+    {
+        public string FileName { get; set; }
+        public string[] Messages { get; set; }
+
+        public LogUpdatedEventArgs()
+        {
+        }
+
+        public LogUpdatedEventArgs(string fileName, string[] messages)
+        {
+            FileName = fileName;
+            Messages = messages;
+        }
+    }
+}
